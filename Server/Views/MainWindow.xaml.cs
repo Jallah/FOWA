7236c489cf9d2fb5ca39aeae47f748650b0eaaf1
Jallah <puncher@gmx.net>
2013-03-22 11:51:08 +0100

    add debug code to server

--- Server/Views/MainWindow.xaml.cs
+++ Server/Views/MainWindow.xaml.cs
@@ -35,11 +35,23 @@ namespace Server.Views
             _service.StartServer();
         }
 
-        public void OnIncomingLoginMessage(object sender, IncomingMessageEventArgs args)
+        public async void OnIncomingLoginMessage(object sender, IncomingMessageEventArgs args)
         {
             // folgendes würde zu folgender Aushahme führen:
             // Der aufrufende Thread kann nicht auf dieses Objekt zugreifen, da sich das Objekt im Besitz eines anderen Threads befindet."
             // fowaServerLogTextBlock.Text = args.Message;
+#if DEBUG
+            List<Friend> testList = new List<Friend>();
+            Friend f = new Friend{Email = "friend@gmx.net", Nick = "Friend 1", UserId = 23};
+            Friend f2 = new Friend{Email = "friend2@google.com", Nick = "Friend 2", UserId = 233};
+            testList.Add(f);
+            testList.Add(f);
+            FriendListMessage me = new FriendListMessage(new User { Email = "degug@test.de", LastMessage = DateTime.Now, Nick = "Testuser" }, testList);
+            bool ok = await args.FowaClient.WriteToClientStreamAync(me);
+            return;
+#endif
+
+
 
             var login = XmlDeserializer.GetLoginInfo(args.Message);
             Dispatcher.BeginInvoke(new Action(() => fowaServerLogTextBlock.Text += "Incoming Login:\n" + '\t' + login.Email + "\n\t" + login.Pw + "\n\n"));
@@ -48,7 +60,7 @@ namespace Server.Views
             if (!_userFriendService.UserExists(login.Email))
             {
                 Dispatcher.BeginInvoke(new Action(() => fowaServerLogTextBlock.Text += "Login failed: User not found.\n----------\n"));
-                args.FowaClient.WriteToClientStreamAync(new ErrorMessage(ErrorMessageKind.LiginError, "User not found."));
+                bool successful = await args.FowaClient.WriteToClientStreamAync(new ErrorMessage(ErrorMessageKind.LiginError, "User not found."));
                 return;
             }
 
@@ -58,7 +70,7 @@ namespace Server.Views
             if (!user.pw.Equals(login.Pw))
             {
                 Dispatcher.BeginInvoke(new Action(() => fowaServerLogTextBlock.Text += "Login failed: Wrong Password.\n----------\n"));
-                args.FowaClient.WriteToClientStreamAync(new ErrorMessage(ErrorMessageKind.LiginError, "Incorrect Password"));
+                bool successfull = await args.FowaClient.WriteToClientStreamAync(new ErrorMessage(ErrorMessageKind.LiginError, "Incorrect Password"));
                 return;
             }
 
@@ -68,7 +80,7 @@ namespace Server.Views
             FriendListMessage m = new FriendListMessage(new User { Email = user.email, LastMessage = user.lastMessage, Nick = user.nick, UserId = user.ID }, friendList);
 
             // send FriendListMessage
-            args.FowaClient.WriteToClientStreamAync(m);
+            bool success = await args.FowaClient.WriteToClientStreamAync(m);
         }
 
 
